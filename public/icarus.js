e36dffdec229da458a73e9d6a64f408f84e9e0b5
wenchen1114 <kwc354@nyu.edu>
2020-10-22 17:04:26 -0400

    error fix

--- public/icarus.js
+++ public/icarus.js
@@ -22,7 +22,7 @@ doc.addEventListener('input', function(e){
 setInterval(readytoSubmit, 1000);
 
 function readytoSubmit(){
-  if(progress.value > 210){
+  if(progress.value > 295){
     document.getElementById("submit").style.visibility = "visible";
   }
 }
